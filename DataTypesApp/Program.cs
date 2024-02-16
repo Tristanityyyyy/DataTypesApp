4a65915c21d74d1935977d44a081ba854d53d871
Labjata.315393 <Labjata.315393@LAB-B-PC14>
2024-02-16 09:36:51 +0800

    Add project files.

---
+++ DataTypesApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Dynamic;
+
+namespace DataTypesApp
+{
+    class DataTypesProgram {
+        static void Main(string []args) {
+            Console.Write("Enter the pieces of apple: ");
+            int pieces = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the total price of " + pieces + " apple(s): ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of " + pieces + " apple(s) is " + price);
+            Console.WriteLine("The value of original price is " + price);
+            Console.WriteLine("The value of converted price is " +(int) price);
+            Console.WriteLine("\n Press any key to exit");
+            Console.ReadKey();
+        }
+    
+    }
+
+}
